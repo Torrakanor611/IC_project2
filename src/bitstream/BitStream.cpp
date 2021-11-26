728a2d3e4be7ce7c3d1915d2cea8e82490ed656f
Pedro Abreu <pedro.abreu@ua.pt>
2021-11-26 22:44:03 +0000

    update

--- src/bitstream/BitStream.cpp
+++ src/bitstream/BitStream.cpp
@@ -2,10 +2,32 @@
 
 using namespace std;
 
-unsigned char readBit();
 
-void writeBit(unsigned char bit);
+unsigned char BitStream::readBit(){
+    if(file.is_open()){
+        if (bit == 0){
+            bit = 7;
+            file.read((char*)&buffer , 1);
+            return (buffer >> bit) & 0x01; 
+        }
+        bit--;
+        return (buffer >> bit) & 0x01;
+    }
+}
 
-unsigned char* readNbits(int inipos, int n);
+void BitStream::writeBit(unsigned char bit){
+    ;
+}
 
-void writeNbits(int inipos, int n, unsigned char* bits);
+unsigned char* BitStream::readNbits(int inipos, int n){
+    ;
+}
+
+void BitStream::writeNbits(int inipos, int n, unsigned char* bits){
+    ;
+}
+
+// write buffer on file
+void BitStream::close(){
+    ;
+}
--- src/bitstream/BitStream.hpp
+++ src/bitstream/BitStream.hpp
@@ -1,3 +1,6 @@
+#ifndef BITSTREAM_H
+#define BITSTREAM_H
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,20 +11,26 @@ using namespace std;
 class BitStream {
     public:
         BitStream(string filename, char mode){
-            inoutfs.open(filename);
+            this->file.open(filename, ios::binary);
             if (mode != 'w' && mode != 'r'){
                 cout << "ERROR: invalid mode!" << endl;
                 exit(EXIT_FAILURE);
             }
-            
+            this->bit = 0;
         }
         unsigned char readBit();
         void writeBit(unsigned char bit);
         unsigned char* readNbits(int inipos, int n);
         void writeNbits(int inipos, int n, unsigned char* bits);
+        void close();
     
     private:
-        fstream inoutfs;
-        unsigned char* buffer;              // buffer de trabalho
-        int numBits;
+        fstream file;
+        unsigned char buffer;   // buffer[7] Msb , ... , buffer[0] Lsb
+        int bit; // goes from 0 to 7
+        // bit in 'r' mode points to last read bit
+        // bit in 'w' mode points to last written bit
+
 };
+
+#endif
