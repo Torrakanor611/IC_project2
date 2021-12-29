b554d2815e574f3cc6e47df531086f9c823bc552
Ana Filipe <anafile@ua.pt>
2021-12-29 16:37:23 +0000

    Codec audio doxy

--- src/codecaud/Codecaud.hpp
+++ src/codecaud/Codecaud.hpp
@@ -5,6 +5,10 @@
 #include <sndfile.h>
 #include <vector>
 
+/**
+ * Class for encoding and decoding audio files.
+ */
+
 class Codecaud{
     private:
         char* filename;
@@ -15,11 +19,46 @@ class Codecaud{
         
     public:
         Codecaud();
+
+        /**
+         * Codecaud Class Constructor
+         * @param filename path to a audio file.
+         */
         Codecaud(const char *filename);
+
+         /**
+         * Compress audio file
+         * @param filename path to a file were to store the enconded value.
+         * @param num value to choose the predictor that will be use. 
+         * Choosing 1, the predictor will only consider the previous value; 2 it will consider the 2 previous values; and choosing 3 will consider the 3 previous values.
+         * @param lossy value to choose between lossless (0) and lossy (1) encoding.
+         * @param shamt 
+         */
         void compress(const char *fileDst, int num, bool lossy, int shamt);
+        
+        /**
+         * Lossless Preditor 
+         * @param vetSrc vector that contains all the samples of the audio file.
+         */        
         void preditor(std::vector<short> vetSrc);
+
+        /**
+         * Lossy Preditor 
+         * @param vetSrc vector that contains all the samples of the audio file.
+         * @param shamt 
+         */
         void preditorLossy(std::vector<short> vetSrc, int shamt);
+
+        /**
+         * Decompress audio file
+         * @param fileSrc path to a file were the stored enconded value is.
+         */
         void decompress(const char *fileSrc);
+
+        /**
+         * Show the histograms of the residuals obtained after prediction, as well as the entropy value. 
+         * Compare the values with the entropy of the original data.
+         */  
         void showHistEnt();
 };
 
